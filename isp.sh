84a3cc98861bba14cc622045df92674ceec0b0f7
sysdata <ebrou833@gmail.com>
2025-04-21 09:50:02 +0000

    Update isp.sh

--- isp.sh
+++ isp.sh
@@ -1,13 +1,19 @@
-hostnamectl set-hostname isp.au-team.irpo; exec bash
-
-
-if ! grep -q "net.ipv4.ip_forward = 1" /etc/sysctl.conf; then
-    echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+if [[ $EUID -ne 0 ]]; then
+  echo "Пожалуйста, запускайте этот скрипт с правами root: sudo $0"
+  exit 1
 fi
+
+hostnamectl set-hostname isp.au-team.irpo
+
+
+grep -qxF "net.ipv4.ip_forward = 1" /etc/sysctl.conf || echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 sysctl -p
 
 
-cat > /etc/nftables/isp.nft <<EOF
+mkdir -p /etc/nftables
+
+
+cat >/etc/nftables/isp.nft <<EOF
 table inet nat {
     chain POSTROUTING {
         type nat hook postrouting priority srcnat;
@@ -17,10 +23,10 @@ table inet nat {
 EOF
 
 
-if ! grep -q 'include "/etc/nftables/isp.nft"' /etc/sysconfig/nftables.conf; then
-    echo 'include "/etc/nftables/isp.nft"' >> /etc/sysconfig/nftables.conf
-fi
+NFT_CONF="/etc/sysconfig/nftables.conf"
+INCLUDE_LINE='include "/etc/nftables/isp.nft"'
+grep -Fxq "$INCLUDE_LINE" "$NFT_CONF" || echo "$INCLUDE_LINE" >> "$NFT_CONF"
 
 
 systemctl enable --now nftables
-
+echo -e "\n✅ Готово! NAT настроен. Проверь содержимое файла: /etc/nftables/isp.nft"
