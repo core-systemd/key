8079f0f640ffcc9d9e985409c1f5a4bde0d57e7a
sysdata <ebrou833@gmail.com>
2025-04-21 14:49:20 +0000

    Update add.sh

--- add.sh
+++ add.sh
@@ -2,32 +2,19 @@ useradd net_admin  -U
 passwd net_admin
 
 
+if ! grep -q "^net_admin ALL=(ALL) NOPASSWD: ALL" /etc/sudoers; then
+  echo "➕ Добавляем net_admin в sudoers без пароля..."
+  tmpfile=$(mktemp)
+  cp /etc/sudoers "$tmpfile"
+  echo "net_admin ALL=(ALL) NOPASSWD: ALL" >> "$tmpfile"
 
-LINE_TO_FIND="# %wheel        ALL=(ALL)       NOPASSWD: ALL"
-NEW_LINE="net_admin ALL=(ALL) NOPASSWD: ALL"
-
-if grep -Fxq "$NEW_LINE" /etc/sudoers; then
-  echo "ℹ️ Строка уже существует в sudoers"
-  exit 0
-fi
-
-TMP_FILE=$(mktemp)
-
-awk -v find="$LINE_TO_FIND" -v insert="$NEW_LINE" '
-{
-  print
-  if ($0 == find) {
-    print insert
-  }
-}' /etc/sudoers > "$TMP_FILE"
-
-if visudo -c -f "$TMP_FILE"; then
-  cp "$TMP_FILE" /etc/sudoers
-  echo "✅ Успешно вставлено!"
+  if visudo -c -f "$tmpfile"; then
+    cp "$tmpfile" /etc/sudoers
+    echo "✅ Успешно добавлено!"
+  else
+    echo "❌ Ошибка синтаксиса в sudoers. Изменения не применены."
+  fi
+  rm -f "$tmpfile"
 else
-  echo "❌ Ошибка в sudoers! Не трогаем оригинал."
+  echo "ℹ️ net_admin уже есть в sudoers"
 fi
-
-
-rm -f "$TMP_FILE"
-
