2014fe166adf706dde2c1ff48addae52ed11c4ce
sysdata <ebrou833@gmail.com>
2025-04-21 22:27:17 +0000

    Update hq-srv.sh

--- hq-srv.sh
+++ hq-srv.sh
@@ -61,4 +61,109 @@ systemctl restart sshd
 echo "[+] Настройка завершена."
 
 
+dnf install -y bind bind-utils
 
+
+cp /etc/named.conf /etc/named.conf.bak
+cat > /etc/named.conf << 'EOF'
+//
+// named.conf
+//
+// Provided by Red Hat bind package to configure the ISC BIND named(8) DNS
+// server as a caching only nameserver (as a localhost DNS resolver only).
+//
+
+options {
+	listen-on port 53 { 127.0.0.1; 192.168.100.0/26; 192.168.100.64/28; 192.168.200.0/27; };
+	listen-on-v6 port 53 { none; };
+	directory 	"/var/named";
+	dump-file 	"/var/named/data/cache_dump.db";
+	statistics-file "/var/named/data/named_stats.txt";
+	memstatistics-file "/var/named/data/named_mem_stats.txt";
+	secroots-file	"/var/named/data/named.secroots";
+	recursing-file	"/var/named/data/named.recursing";
+	allow-query     { any; };
+	forwarders	{ 8.8.8.8; };
+	recursion yes;
+	dnssec-validation no;
+	managed-keys-directory "/var/named/dynamic";
+	geoip-directory "/usr/share/GeoIP";
+	pid-file "/run/named/named.pid";
+	session-keyfile "/run/named/session.key";
+	include "/etc/crypto-policies/back-ends/bind.config";
+};
+
+logging {
+	channel default_debug {
+		file "data/named.run";
+		severity dynamic;
+	};
+};
+
+zone "." IN {
+	type hint;
+	file "named.ca";
+};
+
+zone "au-team.irpo" {
+	type master;
+	file "master/au-team.db";
+};
+
+zone "100.168.192.in-addr.arpa" {
+	type master;
+	file "master/au-team_rev.db";
+};
+
+include "/etc/named.rfc1912.zones";
+include "/etc/named.root.key";
+EOF
+
+
+mkdir -p /var/named/master
+
+
+cat > /var/named/master/au-team.db << 'EOF'
+$TTL 1D
+@	IN	SOA	au-team.irpo. root.au-team.irpo. (
+					0	; serial
+					1D	; refresh
+					1H	; retry
+					1W	; expire
+					3H )	; minimum
+	IN	NS	au-team.irpo.	
+	IN	A	192.168.100.2
+hq-rtr	IN	A	192.168.100.1
+br-rtr	IN	A	192.168.200.1
+hq-srv	IN	A	192.168.100.2
+hq-cli	IN	A	192.168.100.66
+br-srv	IN	A	192.168.200.2
+moodle	CNAME	hq-rtr.au-team.irpo.
+wiki	CNAME	hq-rtr.au-team.irpo.
+EOF
+
+
+cat > /var/named/master/au-team_rev.db << 'EOF'
+$TTL 1D
+@	IN SOA	au-team.irpo. root.au-team.irpo. (
+					0	; serial
+					1D	; refresh
+					1H	; retry
+					1W	; expire
+					3H )	; minimum
+
+	IN	NS	au-team.irpo.
+1	IN	PTR	hq-rtr.au-team.irpo.
+2	IN	PTR	hq-srv.au-team.irpo.
+66	IN	PTR	hq-cli.au-team.irpo.
+EOF
+
+
+chown -R root:named /var/named/master
+chmod 0640 /var/named/master/*
+
+
+named-checkconf -z
+systemctl enable --now named
+
+echo "BIND DNS-сервер успешно установлен и настроен."
