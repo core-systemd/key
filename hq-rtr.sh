e27604c5facb94d0bdcfa756498a26dde4d9e5af
sysdata <ebrou833@gmail.com>
2025-04-21 09:11:18 +0000

    Update hq-rtr.sh

--- hq-rtr.sh
+++ hq-rtr.sh
@@ -32,3 +32,52 @@ grep -Fxq "$INCLUDE_LINE" "$NFT_CONF" || echo "$INCLUDE_LINE" >> "$NFT_CONF"
 systemctl enable --now nftables
 
 echo -e "\n✅ Готово! NAT настроен. Проверь содержимое файла: /etc/nftables/hq-rtr.nft"
+
+
+
+if [[ $EUID -ne 0 ]]; then
+  echo "Пожалуйста, запускайте от root: sudo $0"
+  exit 1
+fi
+
+
+echo "[*] Устанавливаем FRR..."
+dnf install -y frr
+
+
+echo "[*] Активируем ospfd..."
+sed -i 's/^ospfd=no/ospfd=yes/' /etc/frr/daemons
+
+
+echo "[*] Включаем и запускаем FRR..."
+systemctl enable --now frr
+
+
+echo "[*] Настраиваем OSPF через vtysh..."
+
+vtysh <<EOF
+configure terminal
+router ospf
+ passive-interface default
+ network 192.168.100.0/26 area 0
+ network 192.168.100.64/28 area 0
+ network 10.10.0.0/30 area 0
+ area 0 authentication
+exit
+interface tun1
+ no ip ospf network broadcast
+ no ip ospf passive
+ ip ospf authentication
+ ip ospf authentication-key password
+exit
+exit
+write
+EOF
+
+
+echo "[*] Перезапускаем FRR..."
+systemctl restart frr
+
+
+echo -e "\n[*] Текущая конфигурация OSPF:"
+vtysh -c "show running-config"
