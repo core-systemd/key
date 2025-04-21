88f4a715243e3fb0eecc1475ed10c0e1b62c7ff2
sysdata <ebrou833@gmail.com>
2025-04-21 19:20:18 +0000

    Update br-rtr.sh

--- br-rtr.sh
+++ br-rtr.sh
@@ -1,3 +1,25 @@
+useradd net_admin  -U
+passwd net_admin
+
+
+if ! grep -q "^net_admin ALL=(ALL) NOPASSWD: ALL" /etc/sudoers; then
+  echo "➕ Добавляем net_admin в sudoers без пароля..."
+  tmpfile=$(mktemp)
+  cp /etc/sudoers "$tmpfile"
+  echo "net_admin ALL=(ALL) NOPASSWD: ALL" >> "$tmpfile"
+
+  if visudo -c -f "$tmpfile"; then
+    cp "$tmpfile" /etc/sudoers
+    echo "✅ Успешно добавлено!"
+  else
+    echo "❌ Ошибка синтаксиса в sudoers. Изменения не применены."
+  fi
+  rm -f "$tmpfile"
+else
+  echo "ℹ️ net_admin уже есть в sudoers"
+fi
+
+
 
 if [[ $EUID -ne 0 ]]; then
   echo "Пожалуйста, запускайте этот скрипт с правами root: sudo $0"
