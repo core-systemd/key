1bb922d82176a5238b5fae1820143a53b3467eda
sysdata <ebrou833@gmail.com>
2025-04-21 21:23:55 +0000

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,20 +1,29 @@
-useradd net_admin  -U
-passwd net_admin
 
 
-if ! grep -q "^net_admin ALL=(ALL) NOPASSWD: ALL" /etc/sudoers; then
-  echo "➕ Добавляем net_admin в sudoers без пароля..."
-  tmpfile=$(mktemp)
-  cp /etc/sudoers "$tmpfile"
-  echo "net_admin ALL=(ALL) NOPASSWD: ALL" >> "$tmpfile"
+echo "[*] Установка пакета dhcp-server..."
+dnf install -y dhcp-server
 
-  if visudo -c -f "$tmpfile"; then
-    cp "$tmpfile" /etc/sudoers
-    echo "✅ Успешно добавлено!"
-  else
-    echo "❌ Ошибка синтаксиса в sudoers. Изменения не применены."
-  fi
-  rm -f "$tmpfile"
-else
-  echo "ℹ️ net_admin уже есть в sudoers"
-fi
+
+echo "[*] Создание конфигурационного файла dhcpd.conf..."
+cp /usr/share/doc/dhcp-server/dhcpd.conf.example /etc/dhcp/dhcpd.conf
+
+
+echo "[*] Настройка параметров DHCP..."
+cat <<EOF > /etc/dhcp/dhcpd.conf
+subnet 192.168.100.64 netmask 255.255.255.240 {
+  range 192.168.100.66 192.168.100.78;
+  option domain-name-servers 192.168.100.2;
+  option domain-name "au-team.irpo";
+  option routers 192.168.100.65;
+  default-lease-time 600;
+  max-lease-time 7200;
+}
+EOF
+
+
+echo "[*] Включение и запуск службы dhcpd..."
+systemctl enable --now dhcpd
+
+
+echo "[*] Проверка статуса службы dhcpd..."
+systemctl status dhcpd --no-pager
