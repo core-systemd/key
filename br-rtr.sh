04a68af5df3494aa283554cf93c9dc0413888588
sysdata <ebrou833@gmail.com>
2025-04-21 09:21:09 +0000

    Update br-rtr.sh

--- br-rtr.sh
+++ br-rtr.sh
@@ -1,5 +1,41 @@
 
 if [[ $EUID -ne 0 ]]; then
+  echo "Пожалуйста, запускайте этот скрипт с правами root: sudo $0"
+  exit 1
+fi
+
+hostnamectl set-hostname br-rtr.au-team.irpo
+
+
+grep -qxF "net.ipv4.ip_forward = 1" /etc/sysctl.conf || echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sysctl -p
+
+
+mkdir -p /etc/nftables
+
+
+cat >/etc/nftables/br-rtr.nft <<EOF
+table inet nat {
+    chain POSTROUTING {
+        type nat hook postrouting priority srcnat;
+        oifname "ens18" masquerade
+    }
+}
+EOF
+
+
+NFT_CONF="/etc/sysconfig/nftables.conf"
+INCLUDE_LINE='include "/etc/nftables/br-rtr.nft"'
+grep -Fxq "$INCLUDE_LINE" "$NFT_CONF" || echo "$INCLUDE_LINE" >> "$NFT_CONF"
+
+
+systemctl enable --now nftables
+nmcli connection modify tun1 ip-tunnel.ttl 64
+echo -e "\n✅ Готово! NAT настроен. Проверь содержимое файла: /etc/nftables/hq-rtr.nft"
+
+
+
+if [[ $EUID -ne 0 ]]; then
   echo "Пожалуйста, запускайте от root: sudo $0"
   exit 1
 fi
@@ -23,8 +59,7 @@ vtysh <<EOF
 configure terminal
 router ospf
  passive-interface default
- network 192.168.100.0/26 area 0
- network 192.168.100.64/28 area 0
+ network 192.168.200.0/27 area 0
  network 10.10.0.0/30 area 0
  area 0 authentication
 exit
