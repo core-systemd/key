1ce7edc7a06cb1220114acd98a8df60397753d89
sysdata <ebrou833@gmail.com>
2025-04-21 14:40:51 +0000

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,30 +1,32 @@
 useradd net_admin  -U
 passwd net_admin
 
+#!/bin/bash
+
 insert="net_admin ALL=(ALL) NOPASSWD: ALL"
 target="# %wheel ALL=(ALL) NOPASSWD: ALL"
 
-if ! grep -q "^$insert" /etc/sudoers; then
+if ! grep -Fxq "$insert" /etc/sudoers; then
   echo "➕ Вставляем строку после '$target'..."
   tmpfile=$(mktemp)
 
   awk -v tgt="$target" -v ins="$insert" '
-    $0 == tgt {
+    {
       print
-      print ins
-      next
+      if ($0 == tgt) {
+        print ins
+      }
     }
-    { print }
   ' /etc/sudoers > "$tmpfile"
 
   if visudo -c -f "$tmpfile"; then
     cp "$tmpfile" /etc/sudoers
     echo "✅ Строка успешно вставлена!"
   else
-    echo "❌ Ошибка синтаксиса в sudoers. Изменения не применены."
+    echo "❌ Ошибка синтаксиса. Изменения не применены."
   fi
 
   rm -f "$tmpfile"
 else
-  echo "ℹ️ Строка уже присутствует в sudoers"
+  echo "ℹ️ Строка уже есть в sudoers"
 fi
