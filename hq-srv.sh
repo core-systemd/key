749fadb122fc8c28c82c31f3dff77c99f4d8fa80
sysdata <ebrou833@gmail.com>
2025-05-22 22:18:03 +0000

    Update hq-srv.sh

--- hq-srv.sh
+++ hq-srv.sh
@@ -145,61 +145,6 @@ chmod 0640 /var/named/master/*
 named-checkconf -z
 systemctl enable --now named
 
-cat <<EOF > /etc/chrony.conf
-# Use public servers from the pool.ntp.org project.
-# Please consider joining the pool (https://www.pool.ntp.org/join.html).
-#server ntp1.vniiftri.ru iburst
-#server ntp2.vniiftri.ru iburst
-#server ntp3.vniiftri.ru iburst
-#server ntp4.vniiftri.ru iburst
-server 127.0.0.1 iburst
-
-# Record the rate at which the system clock gains/losses time.
-driftfile /var/lib/chrony/drift
-
-# Allow the system clock to be stepped in the first three updates
-# if its offset is larger than 1 second.
-makestep 1.0 3
-
-# Enable kernel synchronization of the real-time clock (RTC).
-rtcsync
-
-# Enable hardware timestamping on all interfaces that support it.
-#hwtimestamp *
-
-# Increase the minimum number of selectable sources required to adjust
-# the system clock.
-#minsources 2
-
-# Allow NTP client access from local network.
-#allow 192.168.0.0/16
-
-# Serve time even if not synchronized to a time source.
-#local stratum 10
-
-# Require authentication (nts or key option) for all NTP sources.
-#authselectmode require
-
-# Specify file containing keys for NTP authentication.
-keyfile /etc/chrony.keys
-
-# Save NTS keys and cookies.
-ntsdumpdir /var/lib/chrony
-
-# Insert/delete leap seconds by slewing instead of stepping.
-#leapsecmode slew
-
-# Get TAI-UTC offset and leap seconds from the system tz database.
-leapsectz right/UTC
-
-# Specify directory for log files.
-logdir /var/log/chrony
-
-# Select which information is logged.
-#log measurements statistics tracking
-}
-EOF
-
 timedatectl set-timezone Europe/Samara
 # --- 🧹 Очистка и восстановление ---
 
@@ -243,6 +188,11 @@ named-checkconf -z
 systemctl enable --now named
 nmtui
 timedatectl set-timezone Europe/Samara
+dnf install chrony -y
+nano /etc/chrony.conf
+systemctl enable --now chronyd
+systemctl restart chronyd
+chronyc sources && chronyc clients
 EOF
 
 history -c
